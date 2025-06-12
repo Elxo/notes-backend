caa7b86f145bd53f0847178d3552e60ec75b13bc
Elx <107368743+Elxo@users.noreply.github.com>
2025-06-12 14:07:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const notesRoutes = require('./routes/notes');
